7786965518608094c53bf414939eb117a0f682cd
Sam Edwards <samtedwards@pm.me>
2023-12-30 15:26:17 -0500

    added web deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/<REPO>/',
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
   clearScreen: false,
