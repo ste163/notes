40a693aa58241c0e8ab6ec81c71558ea0fb6953a
Sam Edwards <samtedwards@pm.me>
2024-02-09 20:58:29 -0500

    test on commit

--- src-ui/renderer/reactive/sidebar-create-note.ts
+++ src-ui/renderer/reactive/sidebar-create-note.ts
@@ -12,11 +12,6 @@ interface InputProps extends Props {
   container: Element
 }
 
-/**
- * Render create note button and input
- * inside the sidebar. Note: could pass in
- * the container to allow for reusing the component
- */
 function renderSidebarCreateNote({ title, isSavingNote, error }: Props): void {
   const container = document.querySelector('#sidebar-top-menu')
   if (!container) throw new Error('Unable to find sidebar-top-menu container')
@@ -33,7 +28,7 @@ function renderSidebarCreateNote({ title, isSavingNote, error }: Props): void {
         }),
       html: `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <title>Create note</title>
+          <title>Add</title>
           <path d="M15 4H5V20H19V8H15V4ZM3 2.9918C3 2.44405 3.44749 2 3.9985 2H16L20.9997 7L21 20.9925C21 21.5489 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5447 3 21.0082V2.9918ZM11 11V8H13V11H16V13H13V16H11V13H8V11H11Z"></path>
         </svg>
         <span>Create<span/>
