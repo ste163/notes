6a95487227e77cc37b4c4e8ad8ffef7bf7bd8db8
Sam Edwards <samtedwards@pm.me>
2024-02-09 20:03:04 -0500

    added positive scenario test

--- src-ui/renderer/reactive/sidebar-create-note.spec.ts
+++ src-ui/renderer/reactive/sidebar-create-note.spec.ts
@@ -1,8 +1,9 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 import { Window } from 'happy-dom'
 import { within } from '@testing-library/dom'
 import userEvent from '@testing-library/user-event'
 import { renderSidebarCreateNote } from './sidebar-create-note'
+import * as event from 'event'
 
 // TODO: probably move this to a test-util
 // so that I can have one place to manage all of the window state and types.
@@ -38,6 +39,8 @@ const renderComponent = <T extends { [key: string]: unknown }>(
   return within(window.document.body as unknown as HTMLElement)
 }
 
+const TITLE = 'Note title'
+
 describe('create-note', () => {
   it('throws error if container is not found', () => {
     // TODO: use the render abstraction once its done
@@ -54,16 +57,35 @@ describe('create-note', () => {
     }
   })
 
-  it.todo('if create error, render full input and the error')
+  it('if create error, render full input and the error', () => {
+    const { getByRole } = renderComponent(renderSidebarCreateNote, {
+      noteTitle: TITLE,
+      isCreateNoteLoading: false,
+      createError: 'Error',
+    })
+
+    // TODO: need to get the testing-library expects (toHaveValue()
+    // input renders with title
+    expect(
+      getByRole('textbox', { name: 'Note title' }).getAttribute('value')
+    ).toBe(TITLE)
+
+    // check that buttons are not disabled
+    // expect(getByRole('button', { name: 'Save' })).not.toBeDisabled()
+
+    // TODO: the error message renders (once I get the error message setup)
+  })
 
   it.todo(
-    'renders loading state if isCreateNoteLoading and no error'
-    // ensure the save not button is disabled (and maybe also the cancel button?)
+    'renders loading state if loading and not the error'
+    // ensure the save not button is disabled (AND the cancel button!)
   )
 
   it.todo('renders the loading state and spinner if error is passed in')
 
   it('renders base input that can cancel and submit a note, if not loading and no error', async () => {
+    const eventSpy = vi.spyOn(event, 'createEvent')
+
     const { getByRole, queryByRole } = renderComponent(
       renderSidebarCreateNote,
       {
@@ -73,21 +95,22 @@ describe('create-note', () => {
       }
     )
 
+    // renders input on create click
     const createButton = getByRole('button', { name: 'Create' })
     await userEvent.click(createButton)
     const input = getByRole('textbox', { name: 'Note title' })
-    expect(input).toBeDefined()
 
-    // clicking the cancel button should remove the input
+    // clicking cancel button removes input
     const cancelButton = getByRole('button', { name: 'Cancel' })
-
     await userEvent.click(cancelButton)
     expect(queryByRole('textbox', { name: 'Note title' })).toBeNull()
 
-    // user can enter note, and submit event is called with title
+    // can enter note and submit event is called with title
     createButton.click()
-    await userEvent.type(input, 'My new note')
-
-    // TODO: emits the event when save button clicked
+    await userEvent.type(input, TITLE)
+    await userEvent.click(getByRole('button', { name: 'Save' }))
+    expect(eventSpy).toHaveBeenCalledWith(event.NoteEvents.Create, {
+      title: TITLE,
+    })
   })
 })
