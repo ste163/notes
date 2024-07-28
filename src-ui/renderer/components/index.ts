4eafddbeac1781a4541290410d8ed95a8693f61d
Sam Edwards <samtedwards@pm.me>
2024-07-28 13:45:32 -0400

    removed comment

--- src-ui/renderer/components/index.ts
+++ src-ui/renderer/components/index.ts
@@ -1,8 +1,3 @@
-/**
- * renderer/components are fully reusable, atomic UI components only.
- * These are used to build renderer/reactive components.
- * These components are tested through their use in the reactive components.
- */
 export { Button } from './button'
 export { Dialog } from './dialog'
 export { Input } from './input'
