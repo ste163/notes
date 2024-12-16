07cacc3e378d04f5e05acb4b2d91d5eeae9640f4
Sam Edwards <62910490+ste163@users.noreply.github.com>
2024-12-16 10:55:16 -0500

    Fix/js updater (#102)
    * added comment on issue
    
    * removed rust updating; updated config and Cargo
    
    * add devtools back
    
    * app loads; plugin updater not found
    
    * can log updates from browser
    
    * fresh pnpm-lock

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,39 +1,12 @@
-use tauri_plugin_updater::UpdaterExt;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-    .setup(|app| {
-      let handle = app.handle().clone();
-      tauri::async_runtime::spawn(async move {
-        update(handle).await.unwrap();
-      });
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .unwrap();
-}
-
-async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
-    if let Some(update) = app.updater()?.check().await? {
-      let mut downloaded = 0;
-  
-      // alternatively we could also call update.download() and update.install() separately
-      update
-        .download_and_install(
-          |chunk_length, content_length| {
-            downloaded += chunk_length;
-            println!("downloaded {downloaded} from {content_length:?}");
-          },
-          || {
-            println!("download finished");
-          },
-        )
-        .await?;
-  
-      println!("update installed");
-      app.restart();
-    }
-  
+  tauri::Builder::default()
+  .setup(|app| {
+    #[cfg(desktop)]
+    app.handle().plugin(tauri_plugin_updater::Builder::new().build())?;
     Ok(())
-  }
+  })
+  .plugin(tauri_plugin_shell::init())
+  .run(tauri::generate_context!())
+  .expect("error while running tauri application");
+}
