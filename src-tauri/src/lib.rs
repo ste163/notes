3f776808c7ff2bbc8ab9c59dbefd524d45dfc1ca
Sam Edwards <samtedwards@pm.me>
2024-12-16 10:28:26 -0500

    can log updates from browser

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,11 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+  .setup(|app| {
+    #[cfg(desktop)]
+    app.handle().plugin(tauri_plugin_updater::Builder::new().build())?;
+    Ok(())
+  })
   .plugin(tauri_plugin_shell::init())
   .run(tauri::generate_context!())
   .expect("error while running tauri application");
--- src-ui/tauri/update.ts
+++ src-ui/tauri/update.ts
@@ -2,39 +2,45 @@ import { check } from '@tauri-apps/plugin-updater'
 // import { relaunch } from '@tauri-apps/plugin-process'
 import { logger } from 'logger'
 
+// Note: this code comes from Tauri docs for setting up basic update functionality
 const checkForUpdate = async () => {
   try {
     const update = await check()
-    if (update) {
-      console.log(
-        `found update ${update.version} from ${update.date} with notes ${update.body}`
-      )
-      let downloaded = 0
-      let contentLength = 0
-      // alternatively we could also call update.download() and update.install() separately
-      await update.downloadAndInstall((event) => {
-        switch (event.event) {
-          case 'Started':
-            if (event.data.contentLength)
-              contentLength = event.data.contentLength
-            logger.log(
-              'info',
-              `started downloading ${event.data.contentLength} bytes`
-            )
-            break
-          case 'Progress':
-            downloaded += event.data.chunkLength
-            logger.log('info', `downloaded ${downloaded} from ${contentLength}`)
-            break
-          case 'Finished':
-            logger.log('info', 'download finished')
-            break
-        }
-      })
-      logger.log('info', 'update installed')
-      console.log('update installed')
-      //   await relaunch()
+
+    if (!update) {
+      logger.log('info', 'Running latest version. No updates found.')
+      return
     }
+
+    logger.log(
+      'info',
+      `found update ${update.version} from ${update.date} with notes ${update.body}`
+    )
+
+    let downloaded = 0
+    let contentLength = 0
+    // alternatively we could also call update.download() and update.install() separately
+    await update.downloadAndInstall((event) => {
+      switch (event.event) {
+        case 'Started':
+          if (event.data.contentLength) contentLength = event.data.contentLength
+          logger.log(
+            'info',
+            `started downloading ${event.data.contentLength} bytes`
+          )
+          break
+        case 'Progress':
+          downloaded += event.data.chunkLength
+          logger.log('info', `downloaded ${downloaded} from ${contentLength}`)
+          break
+        case 'Finished':
+          logger.log('info', 'download finished')
+          break
+      }
+    })
+    logger.log('info', 'update installed')
+    console.log('update installed')
+    //   await relaunch()
   } catch (error) {
     logger.log('error', 'Error in tauri updater', error)
   }
