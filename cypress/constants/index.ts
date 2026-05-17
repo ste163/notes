444daaadfcfd95b4d28b25ec0c9b47dd8ed44d15
Sam Edwards <samtedwards@pm.me>
2026-05-17 14:33:32 -0400

    removed cypress

--- cypress/constants/index.ts
+++
@@ -1,5 +0,0 @@
-export { data } from './data'
-export { dimensions } from './dimensions'
-export { locators } from './locators'
-
-export const DEFAULT_WAIT = 600
--- cypress/utils/index.ts
+++
@@ -1,1 +0,0 @@
-export { clearIndexDb } from './clear-indexed-db'
